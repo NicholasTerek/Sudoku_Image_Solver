3bbb11068590bfdd5e9daf3833fc84af47b7c69f
Nicholas Terek <139080309+NicholasTerek@users.noreply.github.com>
2023-12-30 19:50:31 -0500

    Update main.py

--- Image_Processing/main.py
+++ Image_Processing/main.py
@@ -6,16 +6,11 @@ from tensorflow.python.keras.models import load_model
 from typing import List, Tuple, Optional
 import os
 
-#path_To_Image = "./Test_Images/Sudoku.png" #To Do
-
 def solve_image(path_To_Image):
     height_Image = 450 
     width_Image = 450
-    #
-    #parameters -> filepath maybe file name?
-    #return inverse_perspective, or return Errors when steps cant be run
-    model = load_model('C:/Users/nicky/OneDrive/Desktop/Sudoku_Solver/Number_Model/my_model2.keras', compile=False)
-    #print(model.summary())
+
+    model = load_model('./Number_Model/my_model2.keras', compile=False)
     img = cv2.imread(path_To_Image)
     img = cv2.resize(img,(width_Image, height_Image))
     blank_img = np.zeros((height_Image, width_Image, 3), np.uint8)
@@ -40,16 +35,13 @@ def solve_image(path_To_Image):
         imgWarpColoured = cv2.cvtColor(imgWarpColoured, cv2.COLOR_BGR2GRAY)
 
         boxes = split_Boxes(imgWarpColoured)
-        #cv2.imshow("Test", boxes[1])
         numbers = getPrediction(boxes, model)
-        #print(numbers)
-        
+
         img_detected_Digits = blank_img.copy()
         img_detected_Digits = displayNumbers(img_detected_Digits, numbers, color=(255, 0 , 255))
         
         solved_number = listProcessing(numbers)
         
-
         numbers = np.asarray(numbers)
         posArray = np.where(numbers >0,0,1)
 
@@ -59,7 +51,6 @@ def solve_image(path_To_Image):
             print("SUDOKU CANT BE SOLVED")
             pass
 
-        
         solved_number = reverse_listProcessing(solved_number)
         solved_number = solved_number*posArray
         imgSolvedDigits= blank_img.copy()
@@ -75,24 +66,9 @@ def solve_image(path_To_Image):
         cv2.imwrite(modified_path, inverse_perspective)
         print("done")
         return modified_path
-        #new_img_detected_Digits = img_detected_Digits.copy()
-        #new_imgSolvedDigits = imgSolvedDigits.copy()
-        #new_img_detected_Digits = drawGrid(new_img_detected_Digits)
-        #new_imgSolvedDigits = drawGrid(new_imgSolvedDigits)
 
     else: 
         print("SUDOKU NOT FOUND")
         return None
 
-#result = solve_image(path_To_Image)
-#print(result)
-#image_Array = ([img,imgThreshold, imgContours, imgBigContours], [imgWarpColoured, img_detected_Digits, imgSolvedDigits, imgInverseWarpColoured])
-#stack = stackImages(1, image_Array)
-#cv2.imshow("OpenCV Process", stack)
-#cv2.waitKey(0)
-
-#image_Array = ([new_img_detected_Digits,new_imgSolvedDigits, inverse_perspective, blank_img], [blank_img, blank_img, blank_img, blank_img])
-#stack = stackImages(1, image_Array)
-#cv2.imshow("OpenCV Process", stack)
-#cv2.waitKey(0)
 
